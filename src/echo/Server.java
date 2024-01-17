368aff97c1629b7928f46697c446cd7bd9c2433d
ds-2024 <yoonds714@nate.com>
2024-01-17 18:27:21 +0900

    Thread + 소켓통신

---
+++ src/echo/Server.java
@@ -0,0 +1,59 @@
+package echo;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+
+public class Server {
+
+	public static void main(String[] args) throws IOException {
+		
+		//서버소켓 객체 생성
+		
+		ServerSocket serverSocket/*스텍영역*/= new ServerSocket();
+		
+		//InetSocketAddress aaa = new InetSocketAddress("192.168.0.65" , "10001")
+		//serverSocket.bind("192.168.0.65" , "10001");
+		
+		//바인딩
+		serverSocket.bind(new InetSocketAddress("192.168.0.65" , 10001));
+		
+		//서버시작
+		System.out.println("<서버시작>");
+		System.out.println("========================");
+		
+		//반복
+		while(true) {
+		System.out.println("[연결을 기다리고 있습니다.]");
+		
+		
+		//클라이언트가 접속을 하면 accept()가 실행됨. 지켜보기
+		Socket socket/*서버*/ = serverSocket.accept();/*정우성*/ //실행하려고 대기중 client 쪽 connect 되면 accept 됨
+		
+		//출장 thread.start()
+		
+		Thread thread = new ServerThread(socket);
+		thread.start();
+	}
+		
+		
+		
+		/*
+		System.out.println("======================");
+		System.out.println("<서버 종료>");
+		///////
+		
+		//닫기
+		bw.close();
+		br.close();
+		socket.close();
+		serverSocket.close();
+		*/
+		
+		
+		
+	}
+
+}
